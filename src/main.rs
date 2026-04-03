1bc4ad76016e057742a430a2c1949ad2abbbf810
Rafael Perez <rafaelandresph@gmail.com>
2026-04-03 00:38:57 -0400

    fix: spelling

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io::{stdout, BufWriter};
 
 fn main() {
     let stdout = stdout();
-    let message = String::from("Hell I'm Raf!");
+    let message = String::from("Hello I'm Raf!");
     let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
