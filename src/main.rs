6e3a3d7db0cf5e78ffc6bb72760ee13f95c3bd80
Rafael Perez <rafaelandresph@gmail.com>
2026-04-03 01:33:14 -0400

    add comment

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,28 @@ use std::io::{stdout, BufWriter};
 fn main() {
     let stdout = stdout();
     let message = String::from("Hello I'm Raf!");
-    let width = message.chars().count();
+    let width_intro = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
 
-    say(&message, width, &mut writer).unwrap();
+    say(&message, width_intro, &mut writer).unwrap();
+
+
+    let message_clippy = String::from("Hello I'm Clippy!");
+    let width_clippy_intro = message.chars().count();
+    // TODO: Add clippy
+    // Now say something as clippy
+    say(&message_clippy, width_clippy_intro, &mut writer).unwrap();
 }
 
 // First code wars problems with rust :)
-fn no_space(x : String) -> String{
-  return x.replace(" ", "");
-}
+// fn no_space(x : String) -> String{
+//   return x.replace(" ", "");
+// }
 
 // Second codewars problem with rust
 // TODO: FIX THIS METHOD
-fn alphabetic(s: &str) -> bool {
-    let collection = s.to_lowercase().chars().collect();
-    collection.sort_by(|a, b| b.cmp(a));
-}
+// fn alphabetic(s: &str) -> bool {
+//     let collection = s.to_lowercase().chars().collect();
+//     collection.sort_by(|a, b| b.cmp(a));
+// }
